bfc447a47cf71b3471fb8ae304216f19af2d7bc7
leto <justinpraxis@gmail.com>
2020-11-13 17:55:21 +0300

    [add] 456

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,1 +1,2 @@
+pub mod s0456_132_pattern;
 pub mod s1609_even_odd_tree;
---
+++ src/solutions/s0456_132_pattern.rs
@@ -0,0 +1,42 @@
+// IMPLEMENTATION:
+// https://leetcode.com/problems/132-pattern/discuss/94071/Single-pass-C%2B%2B-O(n)-space-and-time-solution-(8-lines)-with-detailed-explanation.
+
+// 1) Each time we store a new number, we first pop out all numbers smaller than that number.
+// 2) The most recently popped number is candidate for s3.
+// 3) Once we encounter any number smaller than s3, we know we found a valid sequence since s1 < s3 implies s1 < s2
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn find132pattern(nums: Vec<i32>) -> bool {
+        let mut s3 = std::i32::MIN;
+        let mut stack = Vec::new();
+        for &num in nums.iter().rev() {
+            if num < s3 {
+                return true;
+            }
+            while let Some(elem) = stack.last() {
+                if num > *elem {
+                    s3 = *elem;
+                    let _ = stack.pop();
+                } else {
+                    break;
+                }
+            }
+            stack.push(num);
+        }
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_456() {
+        assert_eq!(Solution::find132pattern(vec![1, 2, 3, 4]), false);
+        assert_eq!(Solution::find132pattern(vec![3, 1, 4, 2]), true);
+        assert_eq!(Solution::find132pattern(vec![-1, 3, 2, 0]), true);
+    }
+}
